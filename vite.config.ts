ea53468705f01eb93913756cb6a5eb88e0475ec6
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2025-06-27 15:58:29 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [vue()],
   base: "/void-leeba/",
