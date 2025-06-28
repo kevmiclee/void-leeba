1a661c5378e241688317125153862fe172ef6a0a
kevmiclee12 <87021378+kevmiclee12@users.noreply.github.com>
2025-06-28 10:53:48 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/void-leeba/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
